bd77ffb575eeabdebcfce8decef6f3430e645dce
Will <fgnickson@gmail.com>
2019-07-15 07:53:06 +0100

    remove topic buttons

--- src/Components/Articles/Buttons/Topic.js
+++ src/Components/Articles/Buttons/Topic.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "@reach/router";
+// import { Link } from "@reach/router";
 
 export default function TopicButtons() {
   return (
     <div className="container section offset-1 topic-sort-buttons">
-      <Link to="/topics/coding">
+      {/* <Link to="/topics/coding">
         <button className="btn-small waves-effect waves-light">Coding</button>
       </Link>
       <Link to="/topics/football">
@@ -12,7 +12,7 @@ export default function TopicButtons() {
       </Link>
       <Link to="/topics/cooking">
         <button className="btn-small waves-effect waves-light">Cooking</button>
-      </Link>
+      </Link> */}
     </div>
   );
 }
