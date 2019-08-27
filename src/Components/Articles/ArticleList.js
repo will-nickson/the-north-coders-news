1861572402e008969c7943f6c41d75b7c46a6a1e
Will <fgnickson@gmail.com>
2019-08-27 10:59:37 +0100

    restyled voting function

--- src/Components/Articles/ArticleList.js
+++ src/Components/Articles/ArticleList.js
@@ -4,10 +4,9 @@ import { Link } from "@reach/router";
 import Error from "../../Error";
 import { distanceInWords } from "date-fns";
 import SortByButtons from "./Buttons/SortBy";
-import TopicButtons from "./Buttons/Topic";
 import Voter from "../Voter";
 
-export class ArticleList extends Component {
+class ArticleList extends Component {
   state = {
     articles: [],
     sortBy: null,
@@ -20,56 +19,69 @@ export class ArticleList extends Component {
   render() {
     const { articles, hasError, loading } = this.state;
 
-    if (loading) return <p>Loading...</p>;
+    if (loading) return (
+      <div className="container progress">
+        <div className="indeterminate" />
+      </div>
+    );
     if (hasError) return <Error error={hasError} />;
 
     return (
       <div className="container">
-        <SortByButtons
-          setSortBy={this.setSortBy.bind(this.props)}
-          setOrderBy={this.setOrderBy.bind(this.props)}
-        />
+        
+          <SortByButtons
+            setSortBy={this.setSortBy.bind(this.props)}
+            setOrderBy={this.setOrderBy.bind(this.props)}
+          />
 
         {articles.map(article => {
           return (
-            <div className="post col s8 l8 z-depth-1" key={article.article_id}>
-              <div className="voting-wrapper z-depth-1">
-                <div className="voting">
-                  <Voter
-                    votes={article.votes}
-                    article_id={articles.article_id}
-                  />
-                </div>
-              </div>
-              <i className="material-icons icon-comment">insert_comment</i>
-
-              <div className="content">
-                <div className="title">
+            <div
+              className="article-list-post col s8 l8 z-depth-1"
+              key={article.article_id}
+            >
+              <Voter votes={article.votes} article_id={articles.article_id} />
+              <div className="article-list-content-wrapper">
+                <div className="article-list-icon valign-center">
                   <Link to={`/articles/${article.article_id}`}>
-                    {article.title}
+                    <i className="material-icons icon-comment">
+                      insert_comment
+                    </i>
                   </Link>
                 </div>
-                <div className="topic">
-                  t/{article.topic} - Posted{" "}
-                  {distanceInWords(article.created_at, new Date())} ago by{" "}
-                  {article.author}
+                <div className="article-list-info">
+                  <div className="article-list-title">
+                    <Link to={`/articles/${article.article_id}`}>
+                      {article.title}
+                    </Link>
+                  </div>
+                  <div className="article-list-topic">
+                    t/{article.topic} - Posted{" "}
+                    {distanceInWords(article.created_at, new Date())} ago by{" "}
+                    {article.author}
+                  </div>
                 </div>
               </div>
             </div>
           );
         })}
 
-        <div className="col l3 offset-l1">
-          <TopicButtons />
-        </div>
-
-        <div className="container section pagination-wrapper">
+        <div className="pagination-wrapper">
           <ul className="pagination">
             <li
               className="btn-small waves-effect waves-light pagination-left"
               onClick={() => this.changePage(-1)}
             >
               <i className="material-icons">chevron_left</i>
+            </li>
+            <li className=/*"active"*/"waves-effect">
+              <a href="#!">1</a>
+            </li>
+            <li className="waves-effect">
+              <a href="#!">2</a>
+            </li>
+            <li className="waves-effect">
+              <a href="#!">3</a>
             </li>
             <li
               className="btn-small waves-effect waves-light pagination-right"
@@ -84,20 +96,20 @@ export class ArticleList extends Component {
   }
 
   changePage = direction => {
-    this.setState(prevState => ({ page: prevState.page + direction }));
+    this.setState(prevState => ({ page: prevState.page + direction }))
   };
 
   setSortBy = sortBy => {
-    this.setState({ sortBy });
+    this.setState({ sortBy })
   };
 
   setOrderBy = orderBy => {
-    this.setState({ orderBy });
+    this.setState({ orderBy })
   };
 
   componentDidMount() {
     this.fetchArticles();
-  }
+  };
 
   componentDidUpdate(prevProps, prevState) {
     const topicChange = prevProps.topic !== this.props.topic;
@@ -108,7 +120,7 @@ export class ArticleList extends Component {
     if (topicChange || sortByChange || orderBy || pageChange) {
       this.fetchArticles();
     }
-  }
+  };
 
   fetchArticles = () => {
     const { topic } = this.props;
